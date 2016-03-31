9751bf7fc0c6557ebdc89d0000a189eef4cff16d
Cristian Moreno Ruiz <cristian@MacBook-Pro-de-Cristian.local>
2016-03-31 19:43:23 +0200

    App - Created WakeOnShake project from a blank project and added some files that might be useful to read data from accelerometer

---
+++ WakeOnShake/main.c
@@ -0,0 +1,39 @@
+/*
+ * main.c
+ */
+
+/* XDCtools Header files */
+#include <xdc/std.h>
+#include <xdc/cfg/global.h>
+#include <xdc/runtime/System.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Semaphore.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Queue.h>
+
+/* TI-RTOS Header files */
+#include <ti/drivers/PIN.h>
+#include <ti/drivers/pin/PINCC26XX.h>
+
+/* Example/Board Header files */
+#include "Board.h"
+
+/* Driverlib CPU functions, used here for CPUdelay*/
+#include <driverlib/cpu.h>
+
+// Drivers for motion sensor
+#include "Board.h"
+#include "movementservice.h"
+#include "sensor_mpu9250.h"
+#include "sensor.h"
+#include "util.h"
+#include "string.h"
+
+
+int main(void) {
+	
+	return 0;
+}
