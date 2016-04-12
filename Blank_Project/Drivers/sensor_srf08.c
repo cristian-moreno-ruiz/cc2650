e6c8b7106bf5771116fa258e160a5733a5e5cffc
Cristian Moreno Ruiz <cristian@MacBook-Pro-de-Cristian.local>
2016-04-12 20:34:16 +0200

    App - Created files to program drivers for SRF08 sensor rk. Left with blink on move feature working and accelerometer reading code commented . Old files from WakeOnShake moved to WakeOnShake_old

---
+++ Blank_Project/Drivers/sensor_srf08.c
@@ -0,0 +1,9 @@
+// Made by: Cristian Moreno
+
+
+#include "Board.h"
+#include "sensor_mpu9250.h"
+#include "sensor_opt3001.h" // For reset of I2C bus
+#include "sensor.h"
+#include "bsp_i2c.h"
+
---
+++ Blank_Project/Drivers/sensor_srf08.h
@@ -0,0 +1,20 @@
+#ifndef SENSOR_SRF08_H
+#define SENSOR_SRF08_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -----------------------------------------------------------------------------
+ *                                          Includes
+ * -----------------------------------------------------------------------------
+ */
+#include "stdint.h"
+#include "stdbool.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SENSOR_SRF08_H */
