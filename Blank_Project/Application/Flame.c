6042af3cc4ea56cbd332b86ba067262e8b07ea10
Cristian Moreno Ruiz <cristian@MacBook-Pro-de-Cristian.local>
2016-05-15 13:26:58 +0200

    App - Flame detector (Keyes) samples taken right with the ADC in pin DP2, from the Keyes Analog Output. Missing interrupt from Digital Output <<<<<<<<<<<<<<<<< << <<< h is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   Blank_Project/Drivers/sensor_srf08.c #

--- Blank_Project/Application/Flame.c
+++ Blank_Project/Application/Flame.c
@@ -14,6 +14,11 @@
 #include <ti/drivers/PIN.h>
 #include <ti/drivers/pin/PINCC26XX.h>
 
+// ADC files
+#include "driverlib/aux_adc.h"
+#include "driverlib/aux_wuc.h"
+//#include <inc/hw_aux_evctl.h>
+
 /* Example/Board Header files */
 #include "Board.h"
 
@@ -34,13 +39,17 @@
  */
 
 
-
+#define ALS_OUTPUT IOID_23
 
 // Task configuration
 #define FLM_TASK_PRIORITY                      1
 #define FLM_TASK_STACK_SIZE                    2048
 
+// ADC Samples
 
+#define SAMPLECOUNT 8
+#define SAMPLETYPE uint16_t
+#define SAMPLESIZE sizeof(SAMPLETYPE)
 
 /*******************************************************************************
  * TYPEDEFS
@@ -68,6 +77,7 @@ PIN_Config pinTableFlame[] = {
     Board_LED1 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
     Board_BUTTON0 | PIN_INPUT_EN | PIN_PULLUP,
 	Board_BUTTON1 | PIN_INPUT_EN | PIN_PULLUP,
+	ALS_OUTPUT   | PIN_INPUT_DIS | PIN_GPIO_OUTPUT_DIS,
     PIN_TERMINATE
 };
 
@@ -117,8 +127,13 @@ void Flame_init(void){
     PIN_setOutputValue(pinHandle, Board_LED1, 1);
 	Task_sleep(500 * (1000 / Clock_tickPeriod));
 
+	// Set up ADC Config
+	AUXWUCClockEnable(AUX_WUC_MODCLKEN0_ANAIF_M|AUX_WUC_MODCLKEN0_AUX_ADI4_M);
+	AUXADCSelectInput(ADC_COMPB_IN_AUXIO7);
+	AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_2P7_US, AUXADC_TRIGGER_MANUAL);
 
-
+	// Disallow STANDBY mode while using the ADC.
+	// Power_setConstraint(Power_SB_DISALLOW);
 
     PIN_setOutputValue(pinHandle, Board_LED2, 1);
 
@@ -133,6 +148,29 @@ void Flame_taskFxn(UArg arg0, UArg arg1){
 
 	Flame_init();
 
+	//SAMPLETYPE adcSamples[SAMPLECOUNT];
+	SAMPLETYPE singleSample;
+	//uint8_t currentSample = 0;
+
+	while(1){
+			//Sleep 100ms in IDLE mode
+			Task_sleep(100 * 1000 / Clock_tickPeriod);
+
+			// Trigger ADC sampling
+			AUXADCGenManualTrigger();
+
+			Task_sleep(100 * 1000 / Clock_tickPeriod);
+
+			singleSample = AUXADCReadFifo();
+			// Clear ADC_IRQ flag. Note: Missing driver for this.
+			//HWREGBITW(AUX_EVCTL_BASE + AUX_EVCTL_O_EVTOMCUFLAGSCLR, AUX_EVCTL_EVTOMCUFLAGSCLR_ADC_IRQ_BITN) = 1;
+
+
+
+
+			System_printf("%d mv on ADC\r\n",singleSample);
+			System_flush();
+	}
 }
 
 
